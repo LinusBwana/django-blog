2408cf1ce006ae7ae6ff76d879ff0368dfcb6358
LinusBwana <linusbwana49@gmail.com>
2025-08-28 17:08:25 +0300

    Created UserProfile

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .views import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
-admin.site.register(UserProfile)
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name = 'Profile'
+    verbose_name_plural = 'Profile'
+  
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/',blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+
+
+# Creating profile when a new User Signs Up
+# @receiver(post_save, sender=User) # Alternative
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = UserProfile.objects.create(user=instance)
+        user_profile.save()
+        # follow themselves
+        user_profile.follows.add(user_profile)
+        user_profile.save()
+post_save.connect(create_profile, sender=User)
